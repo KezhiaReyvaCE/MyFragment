5560d7eacd8aabafb091c7676691f401889490f7
KezhiaReyvaCE <kezhia_reyva_29rpl@student.smktelkom-mlg.sch.id>
2022-04-18 09:31:07 +0700

    Initial commit

---
+++ app/src/main/java/www/smktelkommlg/myfragment/SplashScreen.java
@@ -0,0 +1,28 @@
+package www.smktelkommlg.myfragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends Activity {
+
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
